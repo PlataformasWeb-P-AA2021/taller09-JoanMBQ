02b0a79d7e4be256f92594a44e0220419abb172a
JoanMBQ <timo_2604@hotmail.com>
2021-06-12 12:51:08 -0500

    taller09

--- taller09/proyectociudad/ordenamiento/models.py
+++ taller09/proyectociudad/ordenamiento/models.py
@@ -12,30 +12,31 @@ class Parroquia(models.Model):
 
 
     def __str__(self):
-        return "%s - tipo: %s" % (self.nombre, 
+        return "Parroquia: %s - Tipo de parroquia: %s" % (
+                self.nombre, 
                 self.tipo)
     
 class Barrio(models.Model):
     num_parques = (
-        (1, 'Uno'),
-        (2, 'Dos'),
-        (3, 'Tres'),
-        (4, 'Cuatro'),
-        (5, 'Cinco'),
-        (6, 'Seis'),
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
         )
 
     nombre = models.CharField("Nombre del barrio", max_length=30)
     num_viviendas = models.IntegerField("Numero de viviendas")
-    num_parques = models.IntegerField("Numero de parques", \
-            choices = num_parques) 
+    num_parques = models.IntegerField("Numero de parques", choices = num_parques) 
     num_edificios = models.IntegerField("Numero de edificios")
     parroquia = models.ForeignKey(Parroquia, related_name='parroquias', 
             on_delete=models.CASCADE)
 
 
     def __str__(self):
-        return "%s - viviendas: %d - parques: %d - edificios: %d" % (self.nombre, 
+        return "Barrio: %s - viviendas: %d - Parques: %d - Edificios: %d" % (
+                self.nombre,
                 self.num_viviendas,
                 self.num_parques,
                 self.num_edificios)
